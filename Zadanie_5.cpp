64a371a38faf97251f6c7dc1a13b516efc5b2755
Paweł <pawel.myn@gmail.com>
2017-10-23 00:22:59 +0200

    Poprawienie kodu, poprawne działanie

--- Zadanie_5.cpp
+++ Zadanie_5.cpp
@@ -10,11 +10,12 @@ int main()
 
 { 
 	const double g = 9.81;
-	double x = 30, v = 10; // x = k¹t
-	double z = ((v*v)*(sin * 2)*(x)) / (g);
+	double x = 30, v = 10;
+	double z = v*v*(sin(2 * x)) / g;
 
 	cout << z << endl;
 
 	system("PAUSE");
 	return 0;
+
 }
