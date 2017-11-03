1741ed52daae7864ec513e68bebbfdf98e002107
Paweł <pawel.myn@gmail.com>
2017-11-03 22:00:50 +0100

    Początek zadania

--- Zadanie_7.cpp
+++ Zadanie_7.cpp
@@ -1,1 +1,16 @@
 //Nowe zadanie
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double x = 4, y = 7, c = 1/3;
+
+	double z = sin(cos(c))*x*y*pow(x, -3);
+	cout << z << endl; 
+	system("pause");
+	return 0;
+}
