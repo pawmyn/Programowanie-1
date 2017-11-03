4ff0cea3e181dddeb29a4eb2b1762f45c90b6a3f
Paweł <pawel.myn@gmail.com>
2017-11-03 22:20:27 +0100

    cd

--- Zadanie_7.cpp
+++ Zadanie_7.cpp
@@ -2,14 +2,17 @@
 #define _USE_MATH_DEFINES
 #include <iostream>
 #include <cmath>
+#include <math.h>
 
 using namespace std;
 
 int main()
 {
-	double x = 4, y = 7, c = 1/3;
+	double x = 4, y = 7, c = 1/3, z;
+	const double M_E;
 
-	double z = sin(cos(c))*x*y*pow(x, -3);
+	double z = sin(cos(c))*x*y*pow(x, -3) + log(2)*x +  log(10)*3*y + 
+		pow(((x + (pow(y*M_E))), (x+2)), (1/3)) ;
 	cout << z << endl; 
 	system("pause");
 	return 0;
