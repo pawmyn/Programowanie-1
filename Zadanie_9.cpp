c5e7b978672239954f74efd9eb17e77f920be4bb
pawmyn <pawel.myn@gmail.com>
2017-12-02 15:36:39 +0100

    Tablice
    Początki - tablice

---
+++ Zadanie_9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+	const int n = 5;
+	int tab[n];
+
+	for (int i = 0; i < n; i++) 
+		tab[i] = 3 * (i + 2); 
+
+	for (int i = 0; i < n; i++)
+		cout << tab[i] << endl; 
+	system("pause");
+
+}
