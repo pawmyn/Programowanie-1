f790440a792b402cc454d34188bfcbe6e38c9e75
pawmyn <pawel.myn@gmail.com>
2017-12-07 19:26:46 +0100

    Liczby pseudolosowe

--- Zadanie_10.cpp
+++ Zadanie_10.cpp
@@ -3,46 +3,44 @@
 #include <cstdlib>
 
 using namespace std; 
-
 int liczba, strzal, i; 
 
 int main() {
 	srand(time(NULL));
 	liczba = rand() % 100 + 1;
 
+	cout << "Wylosowalem dla Ciebie liczbe :)"
+		 << "\nMasz 10 prob zeby zgadnac." << endl; 
+
 	while(liczba != strzal) {
-		cout << "Podaj liczbe od 1-100" << endl;
+		
+		cout << "\nPodaj liczbe od 1-100" << endl;
 		cin >> strzal;
-
-		if (strzal < liczba){
-			system("cls");
-			cout << "Za mala!" << endl;
-			i++;
-			cout << "Proba: " << i << endl;
-		}
-
-		if (strzal > liczba) {
-			system("cls");
-			cout << "Za duza!" << endl;
-			i++;
-			cout << "Proba: " << i  << endl;
-		}
 
 		if (strzal == liczba) {
 			system("cls");
 			cout << "Zgadles! Brawo :)" << endl;
-			cout << "Zgadles za " << i << "razem." << endl;
+			cout << "Zgadles za " << i << " razem." << endl;
 			system("pause");
+		}
+
+		else if (strzal < liczba){
+			system("cls");
+			cout << strzal << " jest za mala!" << endl;
+			i++;
+			cout << "Proba: " << i << endl;
+		}
+
+		else if (strzal > liczba) {
+			system("cls");
+			cout << strzal << " jest za duza!" << endl;
+			i++;
+			cout << "Proba: " << i  << endl;
 		}
 
 		if (i >= 10) {
-			cout << "Jestes ciota" << endl; 
+			cout << "Jestes ciota, heh." << endl; 
 			system("pause");
-		}
-			
+		}	
 	}
-	
-	return 0;
-	system("pause");
-
 }
